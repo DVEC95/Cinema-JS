0566db4b9b169d91681d94be1842c534dd5a8515
David Eastcroft <davideastcroft95@gmail.com>
2020-06-14 14:20:09 +0100

    Adds test for finding films by genre.

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -38,7 +38,11 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, cinema.findTitle("Blade Runner 2049"));
   });
 
-  it('should be able to filter films by genre');
+  it('should be able to filter films by genre', function(){
+    const actual = [moonlight, trainspotting];
+    assert.deepStrictEqual(actual, cinema.findByGenre("drama"));
+  });
+  
   it('should be able to check whether there are some films from a particular year');
   it('should be able to check whether there are no films from a particular year');
   it('should be able to check whether all films are over a particular length');
