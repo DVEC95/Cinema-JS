ce1ed3f278a0954651bf91d0a650cba3937563d9
David Eastcroft <davideastcroft95@gmail.com>
2020-06-14 14:01:36 +0100

    Adds film title method.

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,11 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.titles = function(){
+  const titles = this.films.map((film) => {
+    return film.title;
+  });
+  return titles;
+};
+
 module.exports = Cinema;
